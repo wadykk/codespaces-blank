79d15c60402710f4481adab0284dca9034adbede
wadykk <158468806+wadykk@users.noreply.github.com>
2024-04-14 17:29:21 +0000

    docs: renamed weatherRoute to weatherRouters

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import weatherRoute from "./routes/weatherRoute.js";
+import weatherRoute from "./routes/weatherRoutes.js";
 
   // We will create an express app
   const app = express();
