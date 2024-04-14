72f5d8e9741ef1f18fc0bd5754ce50859c377579
wadykk <158468806+wadykk@users.noreply.github.com>
2024-04-14 17:28:19 +0000

    docs: changing import in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import weatherRoute from "./routes/weatherRoute.ts";
+import weatherRoute from "./routes/weatherRoute.js";
 
   // We will create an express app
   const app = express();
