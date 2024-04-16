d8ec9e0ba52b4810b84fee3f436aba220d41dca0
wadykk <158468806+wadykk@users.noreply.github.com>
2024-04-16 22:18:24 +0000

    docs:

--- dist/src/routes/weatherRoutes.js
+++ dist/src/routes/weatherRoutes.js
@@ -5,6 +5,6 @@ const express_1 = tslib_1.__importDefault(require("express"));
 const weatherController_js_1 = require("../controllers/weatherController.js");
 const validators_js_1 = require("../middleware/validators.js");
 const router = express_1.default.Router();
-router.get("/:city", validators_js_1.validateCityName, weatherController_js_1.getWeatherData);
+router.get('/:city', validators_js_1.validateCityName, weatherController_js_1.getWeatherData);
 exports.default = router;
 //# sourceMappingURL=weatherRoutes.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-import express from "express";
+import express from 'express';
 
-  // We will create an express app
-  const app = express();
- 
-  // The port that the express server will listen on
-  const PORT = 3000;
- 
+// We will create an express app
+const app = express();
 
- app.use(express.json());
- // Start the express server
- app.listen(PORT, () => {
- console.log(`Server is listening on port ${PORT}`);
- })
+// The port that the express server will listen on
+const PORT = 3000;
+
+app.use(express.json());
+// Start the express server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
